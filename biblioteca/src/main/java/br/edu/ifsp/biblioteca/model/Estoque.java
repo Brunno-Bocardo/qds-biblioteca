436cc7858f6d5d9c0bb8d22f3712594f946aa9d9
diegofloriano <slrerfloriano@gmail.com>
2025-09-07 15:49:24 -0300

    estoque

--- biblioteca/src/main/java/br/edu/ifsp/biblioteca/model/Estoque.java
+++ biblioteca/src/main/java/br/edu/ifsp/biblioteca/model/Estoque.java
@@ -4,10 +4,53 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "estoque")
 public class Estoque {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int idEstoque;
+    private int id;
 
-    
+    @Column(nullable = false, unique = true)
+    private String codigoExemplar;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_id", nullable = false)
+    private Livro livro;
+
+    @Column(nullable = false)
+    private boolean disponivel = true;
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigoExemplar() {
+        return codigoExemplar;
+    }
+
+    public void setCodigoExemplar(String codigoExemplar) {
+        this.codigoExemplar = codigoExemplar;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }
--- biblioteca/src/main/java/br/edu/ifsp/biblioteca/repository/EstoqueRepository.java
+++ biblioteca/src/main/java/br/edu/ifsp/biblioteca/repository/EstoqueRepository.java
@@ -1,5 +1,10 @@
 package br.edu.ifsp.biblioteca.repository;
 
-public class EstoqueRepository {
+import br.edu.ifsp.biblioteca.model.Estoque;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface EstoqueRepository extends JpaRepository<Estoque, Integer> {
+    Optional<Estoque> findByCodigoExemplar(String codigoExemplar);
 }
