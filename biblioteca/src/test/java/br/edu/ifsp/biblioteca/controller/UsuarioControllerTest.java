e66e3a82e3e1269a3c1f7157c47a1aed5d563d28
Brunno Bocardo <brunno.pb@outlook.com>
2025-12-13 14:28:51 -0300

    modificações no pom e testes em userController

---
+++ biblioteca/src/test/java/br/edu/ifsp/biblioteca/controller/UsuarioControllerTest.java
@@ -0,0 +1,62 @@
+package br.edu.ifsp.biblioteca.controller;
+
+import br.edu.ifsp.biblioteca.dto.UsuarioCreateDto;
+import br.edu.ifsp.biblioteca.model.Usuario;
+import br.edu.ifsp.biblioteca.service.UsuarioService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.List;
+import java.util.Map;
+
+public class UsuarioControllerTest {
+
+    private UsuarioService usuarioService;
+    private UsuarioController usuarioController;
+
+    @BeforeEach
+    void setUp() {
+        usuarioService = Mockito.mock(UsuarioService.class);
+        usuarioController = new UsuarioController(usuarioService);
+    }
+
+    @Test
+    void testCriarUsuarioSucesso() {
+        UsuarioCreateDto inputDto = new UsuarioCreateDto();
+        Usuario usuarioSalvo = new Usuario();
+        Mockito.when(usuarioService.criarUsuario(any(UsuarioCreateDto.class))).thenReturn(usuarioSalvo);
+        ResponseEntity<Usuario> response = usuarioController.criarUsuario(inputDto);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(usuarioSalvo, response.getBody());
+    }
+
+    @Test
+    void testExibirUsuariosSucesso() {
+        Mockito.when(usuarioService.listarUsuarios()).thenReturn(List.of(new Usuario(), new Usuario()));
+        ResponseEntity<List<Usuario>> response = usuarioController.exibirUsuarios();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(2, response.getBody().size());
+    }
+
+    @Test
+    void testDeletarUsuarioSucesso() {
+        String cpf = "12345678900";
+        ResponseEntity<Map<String, String>> response = usuarioController.deletarUsuario(cpf);
+
+        Mockito.verify(usuarioService).deletarUsuario(cpf);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("Usuário deletado!", response.getBody().get("message"));
+    }
+}
